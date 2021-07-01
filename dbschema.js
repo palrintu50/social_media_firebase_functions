50a5fe8f631fed9b79a0476182cf547142545f95
palrintu50 <palrintu50@gmail.com>
2021-07-01 12:59:41 +0530

    changes

---
+++ dbschema.js
@@ -0,0 +1,13 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2021-07-01T11:46:01.018Z',
+            likeCount: 5,
+            commentCount: 2,
+
+    }
+
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,53 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-
+const express = require('express');
+const app = express();
 
 admin.initializeApp();
 
-
-exports.getScream = functions.https.onRequest((req , res) =>{
-    admin.firestore().collection('Screams').get()
+app.get('/screams',(req , res) =>{
+    admin
+    .firestore()
+    .collection('Screams')
+    .orderBy('createdAt ', 'desc')
+    .get()
     .then(data =>{
         let Screams = [];
         data.forEach(doc => {
-            Screams.push(doc.data());
+            Screams.push({
+                screamId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt,
+            });
 
         });
         return res.json(Screams);
     })
     .catch(err => console.error(err))
 
-} );
+})
 
-exports.createScream = functions.https.onRequest((req , res) =>{
+
+app.post('/screams', (req , res) =>{
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     }
-    admin.firestore
+    admin
+        .firestore
         .collection('screams')
         .add(newScream)
         .then(doc => {
-            res.json({ message: 'document ${doc.id} created successfully'})
+            res.json({ message: 'document {doc.id}created successfully'})
         })
-
+        .catch(err => {
+            res.status(500).json({error: 'something went wrong'});
+            console.error(err);
+        })
 } );
+
+
+
+exports.api = functions.https.onRequest(app);
